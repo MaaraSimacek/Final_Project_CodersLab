dfe3b310470a6db032fbe028edfea8a3104591d2
MaaraSimacek <sima.slavoj@seznam.cz>
2025-08-10 13:31:56 +0200

    Movies-Genres-DONE

--- src/Movies/Movies.jsx
+++ src/Movies/Movies.jsx
@@ -1,32 +1,42 @@
 import { useState, useEffect } from "react"
 import { FaStar, FaHeart } from "react-icons/fa";
 import { Search } from "./Search"
+import genres from "./genres"
 import "./Movies.css"
 
 export function Movies() {
-  const [movieData, setMovieData] = useState(null)
+  const [movieData, setMovieData] = useState([])
+  const [selectedGenreId, setSelectedGenreId] = useState(null)
 
-  useEffect( () => {
+  useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=b2cb3eac139e8e545436d02742b28241&page=1`)
-    .then(response => response.json())
-    .then(data => {
-      setMovieData(data.results)
-      console.log(data)
-    }).catch(error => console.log(error) )
+      .then(response => response.json())
+      .then(data => setMovieData(data.results))
+      .catch(error => console.log(error))
   }, [])
+
+  // Filtrování filmů podle žánru
+  const filteredMovies = selectedGenreId
+    ? movieData.filter(movie => movie.genre_ids.includes(selectedGenreId))
+    : movieData
 
   return (
     <section className="movies__content">
+
       <h1>Movies</h1>
 
-      <button>Kategorie</button>
-      <button>Kategorie</button>
-      <button>Kategorie</button>
-      <button>Kategorie</button>
-      <button>Kategorie</button>
-      <button>Kategorie</button>
+      {genres.map(genre => (
+        <button
+          key={genre.id}
+          onClick={() => setSelectedGenreId(genre.id)}
+          className={`${selectedGenreId === genre.id ? "active" : ""} movies__genre-button`} 
+        >
+          {genre.name}
+        </button>
+      ))}
+      <button className="movies__genre-button" onClick={() => setSelectedGenreId(null)}>All Movies</button>
 
-      <Search movies={movieData}/>
+      <Search movies={filteredMovies} />
 
     </section>
   )
---
+++ src/Movies/genres.js
@@ -0,0 +1,10 @@
+const genres = [
+  {id: 28, name: "Action"}, 
+  {id: 18, name: "Drama"}, 
+  {id: 36, name: "History"}, 
+  {id: 14, name: "Fantasy"}, 
+  {id: 35, name: "Comedy"}, 
+  {id: 53, name: "Thriller"},
+]
+
+export default genres
